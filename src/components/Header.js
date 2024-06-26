e9b8b9b27edbd4b4329b8040f0927a0329d61038
uhasri68 <uhasri.tumu@gmail.com>
2024-06-26 13:55:35 +0530

    created language constatnts for search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,8 @@ import { useSelector } from 'react-redux';
 import {addUsers , removeUser} from '../utilis/userSlice';
 import { useDispatch } from 'react-redux';
 import { toggleGptSearch } from '../utilis/gptSlice';
+import { SUPPORTED_LANGUAGES } from '../utilis/constatnts';
+import { changeLanguage } from '../utilis/configSlice';
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -43,6 +45,9 @@ const Header = () => {
   dispatch(toggleGptSearch());
 
 }
+const headerLanguageChange = (e) => {
+  dispatch(changeLanguage(e.target.value))
+}
   return (
 
     <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between'>
@@ -51,7 +56,13 @@ const Header = () => {
          
          
        {user && <div className='flex p-3'>
-       
+        <select className='p-2 bg-gray-900 text-white' onChange ={headerLanguageChange}>
+          {SUPPORTED_LANGUAGES.map((lang) =>
+                    <option key ={lang.indentfier} value={lang.indentfier}>{lang.name}</option>
+
+           )}
+        </select>
+
        <button className='py-2 px-4 mx-4 bg-purple-500 text-white rounded-md' onClick={handleGptSearch}>GPT Search</button>
      
        <img className='w-10 h-10' alt="logo-icon" src={user?.photoURL}></img>
--- src/components/SearchPageBar.js
+++ src/components/SearchPageBar.js
@@ -1,18 +1,26 @@
 import React from 'react'
+import lang from "../utilis/languageConstants"
+import { useSelector } from 'react-redux'
 
 const SearchPageBar = () => {
+
+ const langkey = useSelector(store => store.config.lang)
+
+  // fallback to 'en' if langkey is not available in our translations
+  const translations = lang[langkey] || lang['en']
+
   return (
     <div className='pt-[10%] flex justify-center'>
       <form className='bg-black w-1/2 grid grid-cols-12'>
         <input type='text' className='p-4 m-4 col-span-9'
-         placeholder='What would you like to watch today?'
+         placeholder= {translations.gptSearchPlaceholder || ''}
         ></input>
-        <button className='col-span-3  m-4 py-2 px-4 bg-red-600 text-white rounded-lg'>Search</button>
-
+        <button className='col-span-3 m-4 py-2 px-4 bg-red-600 text-white rounded-lg'>
+          {translations.search || ''}
+        </button>
       </form>
-
     </div>
   )
 }
 
-export default SearchPageBar
+export default SearchPageBar;
